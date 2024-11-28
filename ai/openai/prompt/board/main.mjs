a1b53d83ce0ce26ee411a5e36508e53f734b75f8
anlici <2115693946@qq.com>
2024-11-28 10:58:16 +0800

    select

---
+++ ai/openai/prompt/board/main.mjs
@@ -0,0 +1,8 @@
+const user_messages = [
+    "La performance du système est plus lente que d'habitude.",  //系统的性能比平时慢         
+    "Mi monitor tiene píxeles que no se iluminan.",              //我的显示器有些像素点不亮。
+    "Il mio mouse non funziona",                                // 我的鼠标坏了
+    "Mój klawisz Ctrl jest zepsuty",                            // 我的Ctrl键坏了
+    "我的屏幕在闪烁"                    
+  ]
+  
