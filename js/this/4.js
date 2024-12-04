d9757b55ca3fb4cac6f5d15274f681e6b2c88f6f
anlici <2115693946@qq.com>
2024-12-04 21:20:21 +0800

    add 进程+线程

---
+++ js/this/4.js
@@ -0,0 +1,15 @@
+var a = 'global';
+var obj = {
+    a: 1,
+    foo: foo()
+}
+function foo() {
+    console.log(this.a);
+}
+
+//类数组，指的是有length等少数属性的“伪数组”，有nodelist，nodehtml等有
+// 使用 bind 方法将 obj.foo 绑定到 obj 上
+setTimeout(obj.foo.bind(obj), 1000);
+function setTimeout(fn, delay) {
+    //fn();//调用位置
+}
