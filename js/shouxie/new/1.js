4bd51a48b011c168c92615e0f7e30d76933ee987
anlici <2115693946@qq.com>
2024-12-15 11:21:06 +0800

    update new

--- js/shouxie/new/1.js
+++ js/shouxie/new/1.js
@@ -1,14 +1,28 @@
-function Person(name,age) {
-    this.name = name;
-    this.age = age;
-}
+// function Person(name,age) {
+//     this.name = name;
+//     this.age = age;
+// }
 
+// Person.prototype.sayName = function () {
+//     console.log(this.name);
+// }
+// // new 实例化运算符 {} 和Person.prototype 建立联系 方式：
+// // {} 指向 __proto__ object.prototype
+// // 手动__proto__ 指向 Person.prototype ，让实例化的对象可以访问 Person.prototype 上的方法
+
+// const awei = new Person('awei辣',18);
+// const bwei = new Person('bwei酸',19);
+// console.log(awei);
+
+function Person(name) {
+    this.name = name;
+}
 Person.prototype.sayName = function () {
     console.log(this.name);
 }
-// new 实例化运算符 {} 和Person.prototype 建立联系 方式：
-// {} 指向 __proto__ object.prototype
-// 手动__proto__ 指向 Person.prototype ，让实例化的对象可以访问 Person.prototype 上的方法
-// 构造函数 this指向{} 
-const awei = new Person('awei',18);
-console.log(awei);
+const awei = new Person('张三');
+const bwei = new Person('李四');
+
+console.log(awei.name); // 张三 调用属性
+bwei.sayName()  // 李四 调用新增方法
+
--- js/shouxie/new/2.js
+++ js/shouxie/new/2.js
@@ -4,13 +4,11 @@ function Person(name,age) {
 }
 
 Person.prototype.sayName = function () {
-    console.log(this.name);
+    console.log('名字是'+this.name);
 }
-// 写函数，实现
-
+// 实现一个objectFactory 函数，创建一个对象，注意：不能使用new 关键字
 function objectFactory() {
-    console.log(arguments);
-    
+   
     // 1.创建一个空对象
     // let obj = {};
     const obj = new Object(); // 空对象
@@ -18,18 +16,24 @@ function objectFactory() {
     // this 指向 obj ，argument虽然
     // call 方法改变this指向, 让构造函数中的this 指向obj 
     // [].shift 数组调用shift ，使用.call 方法改变this指向,指向arguments ，call 内部转变为数组
-    // 为什么每次删除第一个元素？ 因为第一个元素是构造函数，删除第一个元素，剩下的元素就是构造函数的参数
+    //objectFactory 为什么每次删除第一个元素？ 因为第一个元素是构造函数，删除第一个元素，剩下的元素就是构造函数的参数
     const Constructor = [].shift.call(arguments); //arguments 是类数组,所有参数的数值，shift 方法删除第一个元素
-    console.log(arguments,Constructor);
+   // console.log(arguments,Constructor);
 // apply 一次传入数组
     Constructor.apply(obj,arguments);//改变this指向,使用apply 方法改变this指向,指向arguments ，apply 内部转变为数组
     // 为什么传入apply两个参数？ 第一个参数是this指向，第二个参数是数组
     // 引用关系
-    // 为啥还有一个__proto__ 指向 Person.prototype ？？
     obj.__proto__ = Constructor.prototype;// 手动__proto__ 指向 Person.prototype ，让实例化的对象可以访问 Person.prototype 上的方法
     return obj;
 }
 
-let awei = objectFactory(Person,'awei',18);
-console.log(awei.name);
+
+function objectFactory2(Fun,...args) {
+    let obj = {};
+    obj.__proto__ = Fun.prototype;
+    Fun.apply(obj,args);
+    return obj;
+}
+let awei = objectFactory(Person,'张三',18);
+console.log(awei.name,awei.__proto__);
 awei.sayName();
