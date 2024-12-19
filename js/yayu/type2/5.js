3d0d10c91c02150977d9f641dbf4b136c09cb94c
anlici <2115693946@qq.com>
2024-12-19 11:18:55 +0800

    update

--- js/yayu/type2/5.js
+++ js/yayu/type2/5.js
@@ -1,31 +1,31 @@
-// let specialObj = {
-//     valueOf: function () {
-//         console.log('valueOf.....');
-//         return 123;
-//     },
-//     toString: function () {
-//         console.log('toString.....');
-//         return '456';
-//     }
-// }
-// console.log(specialObj + 1); // 124
-// // 拦截，使用自定义的valueOf方法
-// // 
-// // 拿到 Number 的值 
-// console.log(Number(specialObj)); // 123
-// console.log(String(specialObj)); // 456
-
-let objectWithoutPrimitiveValueOf = {
+let specialObj = {
     valueOf: function () {
-        console.log('valueOf 2111.....');
-        return this; // 没有原始值 this 就是对象本身,要使用call吗？
+        console.log('valueOf.....');
+        return 123;
     },
     toString: function () {
-        console.log('Calling string.....');
-        // return '798';
-        return this; 
+        console.log('toString.....');
+        return '456';
     }
 }
+console.log(specialObj + 1); // 124
+// 拦截，使用自定义的valueOf方法
+// 
+// 拿到 Number 的值 
+console.log(Number(specialObj)); // 123
+console.log(String(specialObj)); // 456
+
+// let objectWithoutPrimitiveValueOf = {
+//     valueOf: function () {
+//         console.log('valueOf 2111.....');
+//         return this; // 没有原始值 this 就是对象本身,要使用call吗？
+//     },
+//     toString: function () {
+//         console.log('Calling string.....');
+//         // return '798';
+//         return this; 
+//     }
+// }
 
 // console.log(objectWithoutPrimitiveValueOf + 1);
 
--- js/yayu/type2/7.js
+++ js/yayu/type2/7.js
@@ -1,4 +1,5 @@
-console.log(1 + "1"); // 11  二元运算符 字符串拼接
+
+
 console.log(+"1"); // 1  一元运算符 隐私类型转换number
 
 console.log(+ []) // 0 空字符串number 就是0;
@@ -8,6 +9,9 @@ console.log(+ ['1', '2']); // NaN
 console.log(+ ['1,2,3']); // NaN 
 
 console.log(+ {}); // NaN {} => {} => [object Object] => NaN
+
+
+console.log(1 + "1"); // 11  二元运算符 字符串拼接
 
 console.log([] + {}); // [object Object]
 console.log({} + {} ); // [object Object][object Object]
