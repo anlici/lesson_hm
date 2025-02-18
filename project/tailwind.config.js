cbf47f97553a7fd6baaecc6c9fd192e63c1d7d52
ys指风不买醉 <167427383+anlici@users.noreply.github.com>
2025-02-18 23:06:28 +0800

    add

---
+++ project/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // 'sans': ['PingFangSC-Semibold', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+ 
+
+}
